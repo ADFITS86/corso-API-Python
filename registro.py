9178e0506793b4363f05f92eed5a2718e0ca6269
ADFITS86 <apd1.adifrancesco@its4puntozero.it>
2026-05-26 14:24:08 +0000

    aggiunto lista

--- registro.py
+++ registro.py
@@ -3,3 +3,12 @@ voti.append("5")
 for p in voti:
     print("voto registrato:")
     print (p)
+
+    corso={
+        "nome": "web dev",
+        "studenti":[
+            {"nome": "Luca", "id":1}
+            {"nome": "Sara", "id":2}
+        ]
+    }
+print(corso["studenti"]["1"]["nome"])
